6c2176181d870ed4490f1a8cd76579d8fb38bc4e
Valdi <valdilaurentius@gmail.com>
2023-08-11 21:48:13 -0700

    added stick navbar

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ drawText('Laurentius Valdi Adela');
 
 // The code below will allow you to interact with the letters
 bounceBubbles();
+
+// Function to update the navbar position based on scrolling
+function updateNavbarPosition() {
+    var navbar = document.getElementById("navbar");
+    var distanceFromTop = window.scrollY;
+
+    if (distanceFromTop > 100) { // Adjust this value as needed
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+// Event listener for scroll event
+window.addEventListener("scroll", updateNavbarPosition);
