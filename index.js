4a4cd87269ef461484f856d48762aee260ab0557
Valdi <valdilaurentius@gmail.com>
2023-08-15 22:07:56 -0700

    added the modal functions to index.js

--- index.js
+++ index.js
@@ -17,3 +17,36 @@ function updateNavbarPosition() {
 
 // Event listener for scroll event
 window.addEventListener("scroll", updateNavbarPosition);
+
+// Function to open the modal
+function openModal() {
+    var modal = document.getElementById("contactModal");
+    modal.style.display = "block";
+}
+
+// Function to close the modal
+function closeModal() {
+    var modal = document.getElementById("contactModal");
+    modal.style.display = "none";
+}
+
+// Event listener for clicking on the myImg image
+window.addEventListener("DOMContentLoaded", function () {
+    var myImg = document.querySelector(".rounded-circle");
+    myImg.addEventListener("click", openModal);
+});
+
+// Event listener for clicking on the close button or outside the modal to close it
+window.addEventListener("DOMContentLoaded", function () {
+    var closeBtn = document.querySelector(".close");
+    var modal = document.getElementById("contactModal");
+    
+    closeBtn.addEventListener("click", closeModal);
+
+    window.addEventListener("click", function (event) {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+});
+
